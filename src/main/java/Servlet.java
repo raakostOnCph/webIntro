6ed45b3b56b054db4d97e06399434d59baa63e57
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2020-02-16 20:34:54 +0100

    udskrivning af datastrukturer på en jsp side

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -4,6 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "Servlet",urlPatterns = {"/Servlet"} )
 public class Servlet extends HttpServlet {
@@ -16,7 +20,31 @@ public class Servlet extends HttpServlet {
 
         request.setAttribute("minAtribut",navn);
 
+
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("palle");
+        stringList.add("jon");
+        stringList.add("Arne");
+        stringList.add("tur");
+        stringList.add("nik");
+
+        request.setAttribute("minArayliste",stringList);
+
+
+
+        Map<String, String> stringMap = new HashMap<>();
+
+        stringMap.put("jørgen", "313");
+        stringMap.put("ulla","17");
+        stringMap.put("holger B", "42");
+        stringMap.put("knud", "13");
+
+
+        request.setAttribute("mitMap", stringMap);
+
         request.getRequestDispatcher("Velkommen.jsp").forward(request,response);
+
 
 
     }
--- src/main/webapp/Velkommen.jsp
+++ src/main/webapp/Velkommen.jsp
@@ -5,6 +5,7 @@
   Time: 16.58
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -15,6 +16,43 @@
 <h1> Velkommen , du er nu lokket ind</h1>
 
 Du er logget ind med navnet : ${requestScope.minAtribut}
+
+<br>
+<br>
+
+vi printer lige arraylisten ud:
+
+<br>
+
+<c:forEach var="element" items="${requestScope.minArayliste}">
+
+    ${element}
+    <br>
+
+
+
+</c:forEach>
+
+jamen kan ikke også lige printet værdierne i et ud på hva?
+jo det kan jeg da sikker godt.
+<br>
+
+<c:forEach var="element" items="${requestScope.mitMap}">
+
+
+    ${element.key} , ${element.value}
+    <br>
+
+
+
+</c:forEach>
+
+
+
+
+
+
+
 
 </body>
 </html>
