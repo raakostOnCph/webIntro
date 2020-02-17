4fd57aea7ccc1e0c11965c756ef0bb25ed236873
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2020-02-17 14:48:14 +0100

    arraylister, maps og sets skubbes til jsp siden og printes ud

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.*;
 
 @WebServlet(name = "Servlet",urlPatterns = {"/Servlet"} )
 public class Servlet extends HttpServlet {
@@ -14,7 +15,43 @@ public class Servlet extends HttpServlet {
 
         System.out.println(navn);
 
+
         request.setAttribute("minAtribut",navn);
+
+
+        List<String> liste = new ArrayList<>();
+
+        liste.add("jønke");
+        liste.add("blondie");
+        liste.add("blondie");
+        liste.add("blondie");
+        liste.add("gøjler");
+        liste.add("feehaar");
+
+
+        Map<String,String> map = new HashMap<>();
+
+        map.put("jønke", "58");
+        map.put("blondie", "62");
+        map.put("gøjler", "54");
+        map.put("feehår", "49");
+
+        Set<String> set = new HashSet<>(liste);
+
+
+        request.setAttribute("setRokkere", set);
+
+        request.setAttribute("mapRokkere",map);
+
+
+
+
+
+
+        request.setAttribute("rockerListe",liste);
+
+
+
 
         request.getRequestDispatcher("Velkommen.jsp").forward(request,response);
 
--- src/main/webapp/Velkommen.jsp
+++ src/main/webapp/Velkommen.jsp
@@ -5,6 +5,8 @@
   Time: 16.58
   To change this template use File | Settings | File Templates.
 --%>
+
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -15,6 +17,53 @@
 <h1> Velkommen , du er nu lokket ind</h1>
 
 Du er logget ind med navnet : ${requestScope.minAtribut}
+
+<br>
+<br>
+
+her kommer en liste af de haarde drenge :
+
+<br>
+
+<c:forEach var="element" items="${requestScope.rockerListe}">
+
+    ${element}
+    <br>
+
+
+</c:forEach>
+
+<br>
+<br>
+<br>
+
+så skal vi have lys i lagkagen
+
+<br>
+<br>
+
+<c:forEach var="element" items="${requestScope.mapRokkere}">
+
+
+            ${element.key} skal have ${element.value} lys i lagkagen !
+            <br>
+</c:forEach>
+
+<br>
+<br>
+
+her kommer der lige et set med rokkkere
+
+<br>
+
+<c:forEach var="element" items="${requestScope.setRokkere}">
+
+    ${element}
+    <br>
+
+</c:forEach>
+
+
 
 </body>
 </html>
